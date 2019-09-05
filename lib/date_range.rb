3d0277de04ee1e37b06e20f32b0c2eed021151c1
Dominique <dom_taylor@icloud.com>
2019-09-04 17:04:54 -0700

    throws an argument error if start date is after end date date_range_test.rb

--- lib/date_range.rb
+++ lib/date_range.rb
@@ -2,9 +2,19 @@ module Hotel
   class Date_Range
     attr_reader  :start_date,  :end_date
 
-    def initialize(start_date, end_date)
+    def initialize(start_date: , end_date:)
       @start_date = start_date
       @end_date = end_date
+
+      raise ArgumentError, "start date cannot be after end date" if start_date > end_date
     end
   end
 end 
+
+
+# expect {Card.new(14, :clovers)}.must_raise ArgumentError
+
+# if (value > 13 || value < 1) || (suit_array.include?(suit) == false)
+#   raise ArgumentError, "You must provide a value 1..13 or suit
+#   (:hearts, :spades, :clubs, :diamonds)"
+# end 
